e544a339f400ca355ec3ee62caf39954cc446225
Rhiannan Berry <rhiannanb97@gmail.com>
2019-12-06 16:11:48 -0500

    testing

--- main.js
+++ main.js
@@ -2,31 +2,53 @@
 var components = {
     "skin":{
         textures: ['1-n', '2-n', '3-n', '4-n', '5-n', '5-n-2'],
+        images: [],
+        pixels:[],
+        tint: '#ffffff',
         index: 0
     },
     "hair":{
         textures: ['Auburn', 'Black', 'Blair', 'Blonde', 'Brown', 'Dark Brown', 'Ginger', 'Grey', 'Light Brown', 'Red'],
+        images: [],
+        pixels:[],
+        tint: '#ffffff',
         index: 0
     },
     "shirt":{
         textures: ['white','blue','volunteer'],
+        images: [],
+        pixels:[],
+        tint: '#ffffff',
         index: 0
     },
     "logo_front":{
         textures: ['','duck','ae','gt'],
+        images: [],
+        pixels:[],
+        tint: '#ffffff',
         index: 0
     },
     "jacket":{
         textures: ['','white','gray','gt', 'ae'],
+        images: [],
+        pixels:[],
+        tint: '#ffffff',
         index: 0
     },
     "logo_back":{
         textures: ['','duck','ae','gt'],
+        images: [],
+        pixels:[],
+        tint: '#ffffff',
         index: 0
     },
 }
 
 var img, canvas, me;
+
+function updateTextureTint(part, val) {
+    components[part].tint = val;
+}
 
 function updateTextureComponent(part, dir) {
     ind = components[part].index;
@@ -34,6 +56,53 @@ function updateTextureComponent(part, dir) {
     ind = (len + ind + dir)%len;
     components[part].index = ind;
     updateTexture();
+}
+
+function loadImages() {
+    for (var key in components) {
+        imgs = [];
+        textures = components[key].textures;
+        for (var t in textures) {
+            if (t == '') continue;
+            path = key + "/" + textures[t] + ".png"
+            if (path.constructor.name !== 'Object') {
+                source = { src: path };
+            }
+        
+            // Resolve source and img when loaded
+            const img = new Image();
+            //img.onerror = () => reject(new Error('Couldn\'t load image'));
+            //img.onload = () => resolve(Object.assign({}, source, { img }));
+
+            img.src = source.src;
+            imgs.push(img);
+
+            img.onload = () => {
+                var canvas = document.createElement("canvas"),
+                    ppixels = [],
+                    rgbPixel,
+                    ctx,
+                    data,
+                    i;
+
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0, img.width, img.height);
+                data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+
+                for (i = 0; i < data.length; i += 4) {
+                    rgbPixel = [];
+                    rgbPixel[0] = data[i];
+                    rgbPixel[1] = data[i + 1];
+                    rgbPixel[2] = data[i + 2];
+                    ppixels.push(rgbPixel);
+                }
+                pixels = ppixels;
+            }
+        }
+        components[key].images = imgs;
+    }
 }
 
 function getFilePaths() {
@@ -72,6 +141,12 @@ function initialize() {
     img = document.createElement('img');
     canvas = document.createElement('canvas');
     me = document.querySelector("#me").getObject3D('mesh');
+
+    loadImages();
+
+    //console.log(components);
+
+    
 
     updateTexture();
 
